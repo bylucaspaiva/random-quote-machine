4d5c3710b2a84ef0cd1ced5e39dc22786b5f4c29
lspaiva <lucasp4iva@gmail.com>
2022-02-09 22:47:29 -0300

    fix: making possible to do an tweet

--- index.js
+++ index.js
@@ -28,15 +28,15 @@ const QUOTES = [
         author: "Aristotle"
     },
     {
-        quote: "\"He who is unable to live in society, or who has no need because he is sufficient for himself, must be either a beast or a god\"",
+        quote: "He who is unable to live in society, or who has no need because he is sufficient for himself, must be either a beast or a god",
         author: "Aristotle"
     },
     {
-        quote: "\"You talk when you cease to be at peace with your thoughts.\"",
+        quote: "You talk when you cease to be at peace with your thoughts.",
         author: "Kahlil Gibran"
     },
     {
-        quote: "\"Man is the only creature who refuses to be what he is.\"",
+        quote: "Man is the only creature who refuses to be what he is.",
         author: "Albert Camus"
     }
 ]
@@ -46,9 +46,21 @@ window.onload = init;
 const generateQuote = () => {
     let quotesSize = QUOTES.length;
     let randomIndex = Math.floor(Math.random() * quotesSize);
+    let randomQuoteData = QUOTES[randomIndex];
 
-    document.getElementById('text').innerText = QUOTES[randomIndex].quote;
-    document.getElementById('author').innerText = QUOTES[randomIndex].author;
+
+    let twitterLink = "https://twitter.com/intent/tweet?hashtags=quotes&text=";
+    // add the text
+    let quoteInApiFormat = randomQuoteData.quote.replace(/ /g, "%20");
+    twitterLink += quoteInApiFormat;
+    // add the author
+    let authorInApiFormat = randomQuoteData.author.replace(/ /g, "%20")
+
+    twitterLink +=  "%20%20-" + authorInApiFormat;
+
+    document.getElementById('text').innerText = randomQuoteData.quote;
+    document.getElementById('author').innerText = randomQuoteData.author;
+    document.getElementById('tweet-quote').href = twitterLink;
 }
 
 function init() {
